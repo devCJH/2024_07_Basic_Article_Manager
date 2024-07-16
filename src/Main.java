7c1a1811b0d6cfcf3577fb654f7f86b1a25b2b1f
devCJH <http-@naver.com>
2024-07-16 20:04:19 +0900

    article detail

--- src/Main.java
+++ src/Main.java
@@ -53,6 +53,28 @@ public class Main {
 					System.out.printf("%d	|	%s\n", article.id, article.title);
 				}
 				
+			} else if (cmd.startsWith("article detail ")) {
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+				
+				Article foundArticle = null;
+				
+				for (Article article : articles){
+					if (id == article.id) {
+						foundArticle = article;
+						break;
+					}
+				}
+				
+				if (foundArticle == null) {
+					System.out.println(id + "번 게시물은 존재하지 않습니다");
+					continue;
+				}
+				
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
